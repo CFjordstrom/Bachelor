20a6426ed41387fe5638760d120a061dff44946c
Christian <46445109+CFjordstrom@users.noreply.github.com>
2024-01-24 10:36:45 +0100

    convert regex back into string

--- src/RegexConversion.fs
+++ src/RegexConversion.fs
@@ -2,7 +2,22 @@ module RegexConversion
 
 open AbSyn
 
-let rec AbSynToString (regex : Regex) : string =
+let rec RegexToString (regex : Regex) : string =
     match regex with
-    | CharLit (c) -> string c
-    | 
+    | Concat (lst)  -> ConcatToString lst
+    | Union (regex1, regex2) -> RegexToString regex1 + "|" + RegexToString regex2
+
+and ConcatToString (lst : Rep list) : string =
+    match lst with
+    | [] -> ""
+    | rep :: reps -> RepToString rep + ConcatToString reps
+
+and RepToString (rep : Rep) : string =
+    match rep with
+    | CharAtom (atom) -> AtomToString atom
+    | ZeroOrMore (atom) -> AtomToString atom + "*"
+
+and AtomToString (atom : Atom) : string =
+    match atom with
+    | CharLit c -> string c
+    | GroupRegex (regex) -> "(" + RegexToString regex + ")"
