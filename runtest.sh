1af5f102c336faf91d8a01949564b5fe57d7bb15
Christian <46445109+CFjordstrom@users.noreply.github.com>
2024-04-30 23:04:36 +0200

    todo + test if a string is accepted by a language

--- runtest.sh
+++ runtest.sh
@@ -5,16 +5,11 @@ if [ ! -f "$project_file" ]; then
   exit 1
 fi
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <input_file>"
-  exit 1
-fi
-
-input_file="tests/$1"
+input_file="tests/$2"
 
 if [ ! -f "$input_file" ]; then
   echo "Error: Input file not found: $input_file"
   exit 1
 fi
 
-dotnet run --project "$project_file" "$input_file"
+dotnet run --project "$project_file" "$1" "$input_file" "$3"
